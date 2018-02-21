7358e270f32947c2868e49e8f7e2352ae8bc7a03
saurabhunde <er.saurabhunde@gmail.com>
2018-02-21 09:12:13 +0530

    services conpleted till v109
    Signed-off-by: saurabhunde <er.saurabhunde@gmail.com>

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,12 +1,16 @@
+import { Ingredient } from "../Shared/ingredient.model";
+
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingredients : Ingredient[];
 
-    constructor(name: string, description: string, imgPath: string) {
+    constructor(name: string, description: string, imgPath: string,ingredient:Ingredient[]) {
         this.name = name;
         this.description = description;
         this.imagePath = imgPath;
+        this.ingredients = ingredient;
     }
 }
 
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,19 @@
 import { EventEmitter } from "@angular/core/";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../Shared/ingredient.model";
 
 export class RecipeService{
 
     recipeSelectedEvent = new EventEmitter<Recipe>();
    private recipes: Recipe[] = [
-        new Recipe('A test recipe1','This is simple test recipe1','./assets/recipe-icon-6.png'), 
-        new Recipe('A test recipe2','This is simple test recipe2','./assets/recipe-icon-6.png')
+        new Recipe('Chicken Fry',
+        'This is recipe for  Chicken fry',
+        './assets/chicken.jpeg',
+        [new Ingredient('chicken',1),new Ingredient('Chicken Masala',1)]), 
+        new Recipe('Burger',
+        'This is recipe for burger',
+        './assets/burger.jpg',
+        [new Ingredient('buns',2),new Ingredient('wada',1)])
       ];
 getRecipes(){
     return this.recipes.slice();
